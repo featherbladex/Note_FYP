5d7385d0e290f345983f8552167b271924bd45b3
featherbladex <57197043+featherbladex@users.noreply.github.com>
2024-02-13 17:13:04 +0000

    reworking frontend

--- backend/models/noteSchema.js
+++ backend/models/noteSchema.js
@@ -11,6 +11,6 @@ const noteSchema = new Schema({
         type: String,
         required: true
     }
-},{timestamps: true})
+},{timestamps : true})
 
 module.exports = mongoose.model('Note', noteSchema)
