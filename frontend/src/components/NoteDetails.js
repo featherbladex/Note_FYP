d7cbdcaea3d65153fa53a0913aea415e20477f92
featherbladex <57197043+featherbladex@users.noreply.github.com>
2024-02-29 16:18:32 +0000

    added delete functionality

--- frontend/src/components/NoteDetails.js
+++ frontend/src/components/NoteDetails.js
@@ -1,12 +1,28 @@
-const NoteDetails = ({ note })=> {
+import { useNoteContext } from "../hooks/useNoteContext"
 
+const NoteDetails = ({ note })=> {
+    const {dispatch} = useNoteContext()
+    const handleDelete = async () => {
+        const response = await fetch('/api/notes/' + note._id,{
+            method: 'DELETE'
+        })
+
+        const json = await response.json()
+
+        if (response.ok){
+            dispatch({type: 'DELETE_NOTE', payload: json})
+        }
+
+
+    }
+    
     return (
         <div className="note-details">
             <h4> {note.note_title}</h4>
             <p>{note.note_body}</p>
             <p>{note.createdAt}</p>
             <p>{note.updatedAt}</p>
-
+            <span onClick={handleDelete}>Delete Note</span>
         </div>
     )
 }
