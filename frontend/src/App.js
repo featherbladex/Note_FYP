0b210d183b7e49c6f44f220a2de2ecbebe91f02b
featherbladex <57197043+featherbladex@users.noreply.github.com>
2024-04-02 12:55:29 +0100

    testing colours for darkmode

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,9 +4,8 @@ import Navbar from './components/Navbar';
 
 
 function App() {
-
   return (
-    <div className="App">
+    <div className="App" >
       <BrowserRouter>
         <Navbar/>
         <div className='pages'>
